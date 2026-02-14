69241135c0e63af82a92a7cf739a7c517b032abb
Subhodeep Pal <subhodeeppal64@gmail.com>
2026-02-14 09:07:17 +0000

    ddeploy.sh file k3s config added to bashrc

--- deploy.sh
+++ deploy.sh
@@ -22,4 +22,10 @@ echo ""
 echo "To see Worker IPs (Dynamic ASG):"
 terraform output -raw worker_ips_command
 echo ""
+KUBE_EXPORT="export KUBECONFIG=$(pwd)/k3s.yaml"
+if ! grep -qF "$KUBE_EXPORT" ~/.bashrc; then
+    echo "$KUBE_EXPORT" >> ~/.bashrc
+    echo "Successfully added KUBECONFIG to ~/.bashrc"
+fi
+export KUBECONFIG=$(pwd)/k3s.yaml
 echo "Use 'kubectl get nodes' to verify the cluster."
